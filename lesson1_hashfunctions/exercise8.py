eca7f125b414f0e0f98447b265196d417f4676ed
nickmura <92566574+nickmura@users.noreply.github.com>
2022-06-29 10:39:51 -0600

    Added to src directory.

--- lesson1_hashfunctions/exercise8.py
+++
@@ -1,9 +0,0 @@
-import secrets
-import hashlib
-
-while(True):
-    p=secrets.token_bytes(64)
-    h=hashlib.sha256(p).hexdigest()
-    if (h[0:4]=='0000'): break
-print(f'input to get 4 leading zeroes: {p.hex()}')
-print(f'hash with 4 leading zeroes: {h}')
